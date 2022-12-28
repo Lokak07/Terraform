957ab64275374055747815960dc5276742ca7340
Lokak07 <63875574+Lokak07@users.noreply.github.com>
2022-12-29 05:05:28 +0530

    Update demofile.tf

--- demofile.tf
+++ demofile.tf
@@ -1,4 +1,8 @@
-module "demomodule" {
-    source = "git::https://github.com/Lokak07/Terraform"
-  
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0b5eea76982371e91"
+    instance_type = "t2.micro"
 }
